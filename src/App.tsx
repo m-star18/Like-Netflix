683c97d45ed3bd02e07310ac43c56be835347215
m-star18 <masutangoribura@gmail.com>
2023-05-02 02:48:13 +0900

    Add Row component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Row } from './components/Row';
+import { requests } from './request';
 
 function App() {
   return (
-    <div className="App"></div>
+    <div className="App">
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+    </div>
   );
 }
 
--- src/axios.js
+++ src/axios.js
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios from './axios'
 
 // create an axios instance from TMDb API
 const instance = axios.create({
