aa14e6b8100abdfe1bf3fefd4b8c71010eef54d5
m-star18 <masutangoribura@gmail.com>
2023-05-02 17:25:19 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Row } from './components/Row';
-import { requests } from './request';
+import "./App.css";
+import { requests } from "./request";
+import { Row } from "./components/Row";
 
 function App() {
   return (
