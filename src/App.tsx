d96dc7ff2e21074cab77d4c622049f7b17269f9f
m-star18 <masutangoribura@gmail.com>
2023-05-02 18:47:47 +0900

    Fix axios.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { Row } from "./components/Row";
 import { Banner } from "./components/Banner";
@@ -17,9 +18,9 @@ function App() {
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
       <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
       <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
-      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
       <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
-      <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} />
+      <Row title="DOcumentaries" fetchUrl={requests.fetchDocumentaries} />
     </div>
   );
 }
